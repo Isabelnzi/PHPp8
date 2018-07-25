7fbfc47af355fc2e8ed9a4be3e4d0c9ac6d8c7ef
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:43:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo8</title>
+  </head>
+  <body>
+    <p>
+      <?php
+ $first = 3 + 4;
+ $second = 5 * 20;
+ $third = 45 % 5;
+ echo 'Opération: '.$first.' Multiplication: '.$second.' Modulo: '.$third;
+ ?>
+</p>
+</body>
